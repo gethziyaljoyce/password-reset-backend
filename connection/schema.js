2301bb763b00ea3423f8e9bbf6c5803ff6559aee
gethziyaljoyce <87714086+gethziyaljoyce@users.noreply.github.com>
2021-11-12 08:54:03 +0530

    Update schema.js

--- connection/schema.js
+++ connection/schema.js
@@ -5,11 +5,11 @@ const schema = {
     registerSchema: joi.object({
         name: joi.string().required(),
         email: joi.string().email().required(),
-        password: joi.string().min(5).max(10).required(),
+        password: joi.string().min(6).max(10).required(),
     }),
     loginSchema: joi.object({
         email: joi.string().email().required(),
-        password: joi.string().min(5).max(10).required(),
+        password: joi.string().min(6).max(10).required(),
     }),
     forgotPasswordSchema: joi.object({
         email: joi.string().email().required(),
